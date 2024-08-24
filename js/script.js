d8797844f22ba2da573bc4a8dc733867e884f9ec
Abdullah <graphicdesigner.abdullah@gmail.com>
2024-08-24 23:25:57 +0600

    Our clients section completed

--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@ tailwind.config = {
     extend: {
       colors: {
         clifford: "#da373d",
-        primaryColor: "#0c081b",
+        primaryColor: "#080514",
         secondaryColor: "#7f00ff",
+        secondaryColorD: "#5100a1",
         secondaryColorT: "#8000ff80",
         secondaryColorTt: "#8000ff4d",
         primaryTextColor: "#ffffff",
