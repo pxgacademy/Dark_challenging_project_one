e52720f4c89285b6d520d79b94b769d419025562
Abdullah <graphicdesigner.abdullah@gmail.com>
2024-08-24 18:53:54 +0600

    Banner section completed

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Tailwindcss cdn
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        primaryColor: "#0c081b",
+        secondaryColor: "#7f00ff",
+        secondaryColorT: "#8000ff80",
+        secondaryColorTt: "#8000ff4d",
+        primaryTextColor: "#ffffff",
+        secondaryTextColor: "#ffffff80",
+        bgColorOne: "#F9F9F9",
+        bgColorTwo: "#F3F3F3",
+      },
+      backgroundPosition: {
+        customBgPosition: "25px 50px",
+      },
+      fontFamily: {
+        interr: ["Inter", "sans-serif"],
+      },
+    },
+  },
+};
+// "use strict";
